3caa51ee954477a5c94edbcfc34cbd0d6130d924
egalosman <ahmedmoha1500@gmail.com>
2024-09-26 14:15:42 +0300

    assignment completed

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    
+    even_list = [x for x in num_list if x % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return [] 
+    else:
+        extended_list = [x + "!" for x in sentence_list]
+        return extended_list
+
+
+
+print(return_evens([2,3,23,5,6,88,13,15]))
+
+print(make_exclamation(["ahmed", "hello", "good morning"]))
